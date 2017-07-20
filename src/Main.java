9078599dc1bcc3135667c818a3423b6dd4204f53
Jennilyn Howell <jennilynhowell@me.com>
2017-07-20 16:01:06 -0400

    Cleaned up some things and implemented tests.

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,37 @@ public class Main {
     public static void main(String[] args) {
         ReversePolishCalc rpc = new ReversePolishCalc();
 
-        rpc.calculate("1,2,+");
-//        String rpnExpression = "2.5,4.8,+";
-//        double expectedResult = 7.3;
-//        double actualResult = rpc.calculate(rpnExpression);
-//        checkResult(rpnExpression, expectedResult, actualResult);
-//
-//        // Write tests for the other operators (-, *, /)
-//
-//        //Uncomment this test which has many operators
-//        rpnExpression = "5,1,2,+,4,*,+,3,-";
-//        expectedResult = 14.0;
-//        actualResult = rpn.calculate(rpnExpression);
-//        checkResult(rpnExpression, expectedResult, actualResult);
+        String rpnExpression = "2.5,4.8,+";
+        double expectedResult = 7.3;
+        double actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "3.0,1.0,-";
+        expectedResult = 2.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "7.2,1.9,*";
+        expectedResult = 13.68;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "9.5,4.75,/";
+        expectedResult = 2;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        rpnExpression = "5,1,2,+,4,*,+,3,-";
+        expectedResult = 14.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
     }
 
-//    private static void checkResult(String expression, double expected, double actual) {
-//        if (expected == actual) {
-//            System.out.println("SUCCESS: " + expression + " is " + expected);
-//        } else {
-//            System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
-//        }
-//    }
+    private static void checkResult(String expression, double expected, double actual) {
+        if (expected == actual) {
+            System.out.println("SUCCESS: " + expression + " is " + expected);
+        } else {
+            System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
+        }
+    }
 }
--- src/ReversePolishCalc.java
+++ src/ReversePolishCalc.java
@@ -1,32 +1,18 @@
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class ReversePolishCalc {
 
-    // You'll need a variable here to keep track of the top of the stack
     private Stack<Double> stackTop = new Stack();
-
-    // The array of the input string split up
     private String[] tokens;
 
-    // The stack
-    //what is the point of this?
-    private String[] stack;
 
     public double calculate(String input) {
 
-        // 1. Use the String split method to split the string into tokens at the commas
         tokens = input.split(",");
 
-        // 2. Allocate a stack as big as the number of tokens
-        //stack =
-
-        //3. write the algorithm
         for(String token : tokens) {
-            System.out.println("I got a token: " + token);
-
-//            double firstInt = 0.0;
-//            double secondInt = 0.0;
 
             switch (token){
                 case "+":
@@ -36,7 +22,7 @@ public class ReversePolishCalc {
 
                 case "-":
                     System.out.println("Now subtracting");
-                    stackTop.push(stackTop.pop() - stackTop.pop());
+                    stackTop.push(-stackTop.pop() + stackTop.pop());
                     break;
 
                 case "*":
@@ -53,14 +39,14 @@ public class ReversePolishCalc {
                 default:
                     double dToken = Double.parseDouble(token);
                     stackTop.push(dToken);
-                    System.out.println("Added!" + dToken);
                     break;
             }
         }
 
         // 4. return the result
-        System.out.println("Your result is: " + stackTop.pop());
-        return stackTop.pop();
+        System.out.println("Your result is: " + stackTop.peek());
+        return stackTop.peek();
+
     }
 
 //    private void push(String number) {
